147c3112e8b6cfceca7eb32dd6261327a61c4a0e
Lauryn Laeticia Kantono <87691653+Laurynnnn@users.noreply.github.com>
2022-10-30 15:21:02 +0300

    Update resource.py

--- tasksapp/users/resource.py
+++ tasksapp/users/resource.py
@@ -1,7 +1,7 @@
-from usersapp.models import User
+from tasksapp.models import User
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource, reqparse
-from usersapp.schemas.app_schemas import UserSchema
+from tasksapp.schemas.app_schemas import UserSchema
 from datetime import datetime
 
 
